d9e9725d6f88edaa7bc13a92099cb987c3663386
Balint Handler <handlerbalint@gmail.com>
2021-06-03 10:06:08 +0200

    attended and hosted events on user page, current user feed

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,6 @@
 class Event < ApplicationRecord
+  validates :title, presence: true
+  validates :date, presence: true
   belongs_to :creator, class_name: 'User', foreign_key: :creator_id
   has_many :attendances, foreign_key: :attended_event_id, dependent: :destroy
   has_many :attendees, through: :attendances
